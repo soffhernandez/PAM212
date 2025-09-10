e1d5a0c056e96a01e3341a7590fabe6d78e7db3c
soffhernandez <124048747@upq.edu.mx>
2025-09-09 23:07:54 -0600

    operadores

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,13 +1,4 @@
-// hola probando comenTAAAARIOS
-var nameuser = 'jonh'
-let lastname = 'carter'
-console.log(lastname)
-
-nameuser= 'pepe';
-
-const PI = 3.1416
-console.log(PI)
-
-//camelcase
-let userName = 'gordon'
-
+let numberone= 60
+let numertwo = 100
+let res = numberone+numertwo
+console.log(res)
