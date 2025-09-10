75fb3c756ee4f5997de8dfed8458fce983fa9151
soffhernandez <124048747@upq.edu.mx>
2025-09-09 18:40:31 -0600

    TIPOS DE DATOS

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,1 +1,33 @@
-document.write(<h1>Hello world</h1>);
+//tipos de datos
+"HELLO WORLD" // String
+'HELLO WORLD' // string comillas simples
+100000 // num int
+10.1 // números decimales
+
+// booleanos
+true
+false
+
+// array
+['joe','ryan','marta']
+[1,2,3,4]
+[true,false,false]
+
+//object
+'ryan'
+70.7
+14
+true
+
+{
+    username: 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "profesional": true;
+}
+{
+    username: 'joe',
+    "score": 0.4,
+    "hours": 14,
+    "profesional": true;
+}
