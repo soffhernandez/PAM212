9b1bd892ec7b758e982de0f705c2827b07691851
soffhernandez <124048747@upq.edu.mx>
2025-09-09 23:19:02 -0600

    condicionales

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,4 +1,8 @@
-let passwordDB = 'pepe123'
-let input =  'kdeidieeoiide'
-let res = input == passwordDB
-console.log(res)
+let score = 70
+if (score > 30){
+    console.log('ocupas practicas más')
+} else if (score > 15){
+    console.log('estás mejorando')
+} else {
+    console.log('ocupas seguir un tutorial ')
+}
