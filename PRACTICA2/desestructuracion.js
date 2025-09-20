51cd00f4d26bbd2560e283f590b048cae513aa80
soffhernandez <124048747@upq.edu.mx>
2025-09-19 18:48:37 -0600

    códigos del inciso A en donde se hace una destructuración

---
+++ PRACTICA2/desestructuracion.js
@@ -0,0 +1,8 @@
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37,
+    direccion: { ciudad: "Qro", pais: "MX" }
+};
+
+const { nombre, edad, direccion: { ciudad, pais } } = persona;
+console.log("Hola me llamo " + nombre + " tengo " + edad + " años y vivo en " + ciudad);
---
+++ PRACTICA2/filtrar.js
@@ -0,0 +1,9 @@
+const productos = [
+{ nombre: "Laptop", precio: 12000},
+{ nombre: "Mouse", precio: 250},
+{ nombre: "Teclado", precio: 750},
+{ nombre: "Monitor", precio: 3000},
+
+];
+
+console.log(nombres);
