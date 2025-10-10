f9f393bba864b6332e8908600699f2545a65caf4
soffhernandez <124048747@upq.edu.mx>
2025-10-09 18:22:27 -0600

    se crearon los tres botones solicitados

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,35 @@
+// Import: Zona de declaraciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
+import React,{useState} from 'react';
 
+
+// Zona del main: Se ponen componentes
 export default function App() {
+
+  const[contador,setContador]=useState(0);
   return (
+
     <View style={styles.container}>
-      <Text>Hola mundo desde React</Text>
+
+      <Text>Contador: {contador} </Text>
+     
+      <Button title = "incrementar " onPress={()=>setContador(contador +1)}/>
+      
       <StatusBar style="auto" />
+      <Button title = "quitar" onPress={()=>setContador(contador -1)}/>
+      <Button title = "reiniciar " onPress={()=>setContador(contador -contador)}/>
+      
+
+
     </View>
   );
 }
 
+
+
+
+// Zona de estilos: Zona donde se da la estética a los componentes y su funcionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
