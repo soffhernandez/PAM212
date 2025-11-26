300e3086e1048b0ad241e913bb69c39941abb1a1
soffhernandez <124048747@upq.edu.mx>
2025-11-25 23:51:39 -0600

    Insert y select funcionando

---
+++ userBD/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import InsertUsuarioScreen from './screens/InsertUsuarioScreen';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <InsertUsuarioScreen />
+    </View>
+  );
+}
---
+++ userBD/controllers/UsuarioController.js
@@ -0,0 +1,67 @@
+import { Usuario } from '../models/Usuario';
+import DatabaseService from '../database/DatabaseService';
+
+export class UsuarioController {
+  constructor() {
+    this.listeners = [];
+  }
+
+  // Inicializar el controlador con el Service
+  async initialize() {
+    await DatabaseService.initialize();
+  }
+
+  // 5.3: Como segunda parte aquí preparamos el controlador para invocar
+  // al servicio de consulta cuando se le indiquen
+  async obtenerUsuarios() {
+    try {
+      const data = await DatabaseService.getAll();
+      return data.map(
+        u => new Usuario(u.id, u.nombre, u.fecha_creacion)
+      );
+    } catch (error) {
+      console.error('Error al obtener usuarios:', error);
+      throw new Error('No se pudieron cargar los usuarios');
+    }
+  }
+
+  // 5.4: Después preparamos la función de crear usuario la cual
+  // primero valida en el modelo que se cumplan las reglas y
+  // luego lo agrega usando el servicio, notifica a los observadores
+  // y retorna
+  async crearUsuario(nombre) {
+    try {
+      // 1. Validar datos
+      Usuario.validar(nombre);
+
+      // 2. Insertar en BD
+      const nuevoUsuario = await DatabaseService.add(nombre.trim());
+
+      // 3. Notifica a los observadores
+      this.notifyListeners();
+
+      // 4. Retornar usuario creado
+      return new Usuario(
+        nuevoUsuario.id,
+        nuevoUsuario.nombre,
+        nuevoUsuario.fecha_creacion
+      );
+    } catch (error) {
+      console.error('Error al crear usuario:', error);
+      throw error;
+    }
+  }
+
+addListener(callback) {
+  this.listeners.push(callback);
+}
+
+removeListener(callback) {
+  this.listeners = this.listeners.filter(l => l !== callback);
+}
+
+notifyListeners() {
+  this.listeners.forEach(callback => callback());
+}
+
+}
